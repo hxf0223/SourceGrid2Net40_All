8c21248d41c1abfce84ff5030afa1960bfeac3f7
黄小锋 <xiaofeng.huang@udantech.com>
2017-02-15 20:32:56 +0800

    example modify

--- SourceGrid.Examples/GridSamples/Extensions/frmSample18.cs
+++ SourceGrid.Examples/GridSamples/Extensions/frmSample18.cs
@@ -57,135 +57,135 @@ namespace WindowsFormsSample
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.planningGrid1 = new SourceGrid.Planning.PlanningGrid();
-            this.splitter1 = new System.Windows.Forms.Splitter();
-            this.listEditor1 = new SourceGrid.ListEditor();
-            this.cbStart = new DevAge.Windows.Forms.TextBoxUITypeEditor();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.cbEnd = new DevAge.Windows.Forms.TextBoxUITypeEditor();
-            this.btReload = new System.Windows.Forms.Button();
-            this.btExport = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.Controls.Add(this.planningGrid1);
-            this.panel1.Controls.Add(this.splitter1);
-            this.panel1.Controls.Add(this.listEditor1);
-            this.panel1.Location = new System.Drawing.Point(4, 32);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(692, 388);
-            this.panel1.TabIndex = 1;
-            // 
-            // planningGrid1
-            // 
-            this.planningGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.planningGrid1.Location = new System.Drawing.Point(191, 0);
-            this.planningGrid1.Name = "planningGrid1";
-            this.planningGrid1.Size = new System.Drawing.Size(501, 388);
-            this.planningGrid1.TabIndex = 3;
-            this.planningGrid1.AppointmentClick += new SourceGrid.Planning.PlanningGrid.AppointmentEventHandler(this.planningGrid1_AppointmentClick);
-            this.planningGrid1.AppointmentDoubleClick += new SourceGrid.Planning.PlanningGrid.AppointmentEventHandler(this.planningGrid1_AppointmentDoubleClick);
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(188, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 388);
-            this.splitter1.TabIndex = 2;
-            this.splitter1.TabStop = false;
-            // 
-            // listEditor1
-            // 
-            this.listEditor1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.listEditor1.Editors = null;
-            this.listEditor1.EnableAdd = true;
-            this.listEditor1.EnableMove = true;
-            this.listEditor1.EnableRefresh = true;
-            this.listEditor1.EnableRemove = true;
-            this.listEditor1.ItemType = null;
-            this.listEditor1.List = null;
-            this.listEditor1.Location = new System.Drawing.Point(0, 0);
-            this.listEditor1.Name = "listEditor1";
-            this.listEditor1.Properties = null;
-            this.listEditor1.Size = new System.Drawing.Size(188, 388);
-            this.listEditor1.TabIndex = 1;
-            this.listEditor1.ListChanged += new System.EventHandler(this.listEditor1_ListChanged);
-            // 
-            // cbStart
-            // 
-            this.cbStart.BackColor = System.Drawing.Color.Transparent;
-            this.cbStart.Location = new System.Drawing.Point(52, 4);
-            this.cbStart.Name = "cbStart";
-            this.cbStart.Size = new System.Drawing.Size(160, 20);
-            this.cbStart.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(4, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Start";
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(220, 4);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(36, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "End";
-            // 
-            // cbEnd
-            // 
-            this.cbEnd.BackColor = System.Drawing.Color.Transparent;
-            this.cbEnd.Location = new System.Drawing.Point(264, 4);
-            this.cbEnd.Name = "cbEnd";
-            this.cbEnd.Size = new System.Drawing.Size(160, 20);
-            this.cbEnd.TabIndex = 4;
-            // 
-            // btReload
-            // 
-            this.btReload.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btReload.Location = new System.Drawing.Point(432, 4);
-            this.btReload.Name = "btReload";
-            this.btReload.Size = new System.Drawing.Size(75, 23);
-            this.btReload.TabIndex = 6;
-            this.btReload.Text = "Reload";
-            this.btReload.Click += new System.EventHandler(this.btReload_Click);
-            // 
-            // btExport
-            // 
-            this.btExport.Location = new System.Drawing.Point(530, 3);
-            this.btExport.Name = "btExport";
-            this.btExport.Size = new System.Drawing.Size(102, 23);
-            this.btExport.TabIndex = 7;
-            this.btExport.Text = "Export Image";
-            this.btExport.UseVisualStyleBackColor = true;
-            this.btExport.Click += new System.EventHandler(this.btExport_Click);
-            // 
-            // frmSample18
-            // 
-            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-            this.ClientSize = new System.Drawing.Size(700, 423);
-            this.Controls.Add(this.btExport);
-            this.Controls.Add(this.btReload);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.cbEnd);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.cbStart);
-            this.Controls.Add(this.panel1);
-            this.Name = "frmSample18";
-            this.Text = "Planning Sample";
-            this.Load += new System.EventHandler(this.frmSample18_Load);
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.planningGrid1 = new SourceGrid.Planning.PlanningGrid();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.listEditor1 = new SourceGrid.ListEditor();
+			this.cbStart = new DevAge.Windows.Forms.TextBoxUITypeEditor();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.cbEnd = new DevAge.Windows.Forms.TextBoxUITypeEditor();
+			this.btReload = new System.Windows.Forms.Button();
+			this.btExport = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.Controls.Add(this.planningGrid1);
+			this.panel1.Controls.Add(this.splitter1);
+			this.panel1.Controls.Add(this.listEditor1);
+			this.panel1.Location = new System.Drawing.Point(5, 34);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(690, 385);
+			this.panel1.TabIndex = 1;
+			// 
+			// planningGrid1
+			// 
+			this.planningGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.planningGrid1.Location = new System.Drawing.Point(229, 0);
+			this.planningGrid1.Name = "planningGrid1";
+			this.planningGrid1.Size = new System.Drawing.Size(461, 385);
+			this.planningGrid1.TabIndex = 3;
+			this.planningGrid1.AppointmentClick += new SourceGrid.Planning.PlanningGrid.AppointmentEventHandler(this.planningGrid1_AppointmentClick);
+			this.planningGrid1.AppointmentDoubleClick += new SourceGrid.Planning.PlanningGrid.AppointmentEventHandler(this.planningGrid1_AppointmentDoubleClick);
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(226, 0);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(3, 385);
+			this.splitter1.TabIndex = 2;
+			this.splitter1.TabStop = false;
+			// 
+			// listEditor1
+			// 
+			this.listEditor1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.listEditor1.Editors = null;
+			this.listEditor1.EnableAdd = true;
+			this.listEditor1.EnableMove = true;
+			this.listEditor1.EnableRefresh = true;
+			this.listEditor1.EnableRemove = true;
+			this.listEditor1.ItemType = null;
+			this.listEditor1.List = null;
+			this.listEditor1.Location = new System.Drawing.Point(0, 0);
+			this.listEditor1.Name = "listEditor1";
+			this.listEditor1.Properties = null;
+			this.listEditor1.Size = new System.Drawing.Size(226, 385);
+			this.listEditor1.TabIndex = 1;
+			this.listEditor1.ListChanged += new System.EventHandler(this.listEditor1_ListChanged);
+			// 
+			// cbStart
+			// 
+			this.cbStart.BackColor = System.Drawing.Color.Transparent;
+			this.cbStart.Location = new System.Drawing.Point(62, 4);
+			this.cbStart.Name = "cbStart";
+			this.cbStart.Size = new System.Drawing.Size(192, 22);
+			this.cbStart.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(5, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(48, 21);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Start";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(264, 4);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 22);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "End";
+			// 
+			// cbEnd
+			// 
+			this.cbEnd.BackColor = System.Drawing.Color.Transparent;
+			this.cbEnd.Location = new System.Drawing.Point(317, 4);
+			this.cbEnd.Name = "cbEnd";
+			this.cbEnd.Size = new System.Drawing.Size(192, 22);
+			this.cbEnd.TabIndex = 4;
+			// 
+			// btReload
+			// 
+			this.btReload.FlatStyle = System.Windows.Forms.FlatStyle.System;
+			this.btReload.Location = new System.Drawing.Point(518, 4);
+			this.btReload.Name = "btReload";
+			this.btReload.Size = new System.Drawing.Size(90, 25);
+			this.btReload.TabIndex = 6;
+			this.btReload.Text = "Reload";
+			this.btReload.Click += new System.EventHandler(this.btReload_Click);
+			// 
+			// btExport
+			// 
+			this.btExport.Location = new System.Drawing.Point(636, 3);
+			this.btExport.Name = "btExport";
+			this.btExport.Size = new System.Drawing.Size(122, 25);
+			this.btExport.TabIndex = 7;
+			this.btExport.Text = "Export Image";
+			this.btExport.UseVisualStyleBackColor = true;
+			this.btExport.Click += new System.EventHandler(this.btExport_Click);
+			// 
+			// frmSample18
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(6, 14);
+			this.ClientSize = new System.Drawing.Size(700, 423);
+			this.Controls.Add(this.btExport);
+			this.Controls.Add(this.btReload);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.cbEnd);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cbStart);
+			this.Controls.Add(this.panel1);
+			this.Name = "frmSample18";
+			this.Text = "Planning Sample";
+			this.Load += new System.EventHandler(this.frmSample18_Load);
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
 
 		}
 		#endregion
